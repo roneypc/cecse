d4eacd2dd149af2906d0fda7da3bb20472080504
Jorge Roberto Neyra <roneypc@gmail.com>
2014-07-25 14:17:54 +0200

    cecse project - first commit

---
+++ cecse/src/main/java/es/indra/cecse/model/dao/UserDAO.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package es.indra.cecse.model.dao;
+
+/**
+ * @author jrneyra
+ *
+ */
+public class UserDAO {
+
+}
---
+++ cecse/src/main/java/es/indra/cecse/model/entity/User.java
@@ -0,0 +1,100 @@
+/**
+ * 
+ */
+package es.indra.cecse.model.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+/**
+ * @author jrneyra
+ */
+@Entity
+public class User extends GenericEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+
+	private String lastName;
+
+	private String login;
+
+	private String password;
+
+	private String email;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the login
+	 */
+	public String getLogin() {
+		return login;
+	}
+
+	/**
+	 * @param login the login to set
+	 */
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
